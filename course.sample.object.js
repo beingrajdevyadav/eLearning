5e72b40cf980ae9f7f0f1c7b1da948eb2ae47875
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-02 00:19:12 +0530

    main section, footer section added. based on requirement images updated .

---
+++ course.sample.object.js
@@ -0,0 +1,42 @@
+course = {
+    course: "html",
+    courseName: "HTML Foundations",
+    courseDescription: "Learn the basics of HTML and build a strong foundation for web development.",
+    courseThumbnailUrl: "https://miro.medium.com/v2/resize:fit:1280/1*wN4fuuYc-MkF2-CTFB1cVg.jpeg",
+    rating: {
+        stars: 4.5,
+        counts: 956,
+    },
+    instructor: {
+        name: "Vania Ma'am     ",
+        imgUrl: "https://i.pinimg.com/736x/70/aa/28/70aa28f678193194b4a023e542ce4775.jpg",
+        gender: female,
+    },
+    courseDuration: {
+        hours: 3,
+        lectures: 15,
+    },
+    coursePrice: 399,
+    courseLevel: {
+        begginer: true,
+        intermediate: false,
+        export: false,
+        allLevel: false,
+    },
+    courseTag: {
+        bestSeller: false,
+        newest: true,
+        popular: false,
+    },
+    courseType: {
+        paid: true,
+        free: false,
+    },
+    courseTopic: "html",
+    courseFeatures: {
+        subtitle: true,
+        quizes: true,
+        codingExercise: true,
+        practiceTest: true,
+    }
+}
