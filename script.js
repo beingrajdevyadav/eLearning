2d63a57a45a870ad43968843761cb4665f507150
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-02-13 17:33:33 +0530

    fetchData() Function Added To Fetch Courses List From Local data.json File.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("👨‍💻 Jai Shree Shyam Baba ❤🙏")
+console.log("👨‍💻 Jai Shree Shyam Baba ❤🙏");
+
+// ---------------------------------------------- 
+// Function To Fetch Data From data.json file
+// ---------------------------------------------- 
+
+async function fetchData() {
+    try {
+        const response = await fetch('data.json');
+        if (!response.ok) {
+            throw new Error("Network response was not ok!")
+        }
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("There was a problem with fetch operation : ", error)
+    }
+}
+
+
+
+
+
